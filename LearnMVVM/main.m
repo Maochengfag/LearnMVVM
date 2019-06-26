e8dcde141037543b1c081b74d485f78e2e099893
Oliver_Mao <maochengfang@126.com>
2019-06-26 14:21:38 +0800

    Initial Commit

---
+++ LearnMVVM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LearnMVVM
+//
+//  Created by Mac on 2019/6/26.
+//  Copyright © 2019年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
